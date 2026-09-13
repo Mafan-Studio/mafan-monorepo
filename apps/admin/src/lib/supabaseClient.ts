c00073e4af20a1155e075f9a6da0dbaa2787437a
Armin <ar.basirian@gmail.com>
2026-09-13 16:37:21 +0200

    change variable name

--- apps/admin/src/lib/supabaseClient.ts
+++ apps/admin/src/lib/supabaseClient.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.UPABASE_SECRET_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY must be set");
+  throw new Error("SUPABASE_URL and UPABASE_SECRET_KEY must be set");
 }
 
 /**
